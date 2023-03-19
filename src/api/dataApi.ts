be4d4d17c8462a38abaf7a126dc10c46ecced464
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-20 02:01:51 +0900

    [#1] feat: 목데이터 api 호출 함수 추가

---
+++ src/api/dataApi.ts
@@ -0,0 +1,26 @@
+import { DataType } from '../types/data.types';
+import axios from 'axios';
+
+export const getDataApi = async (skip: number, limit: number) => {
+  const response = await axios.get('/data/mockData.json');
+  const begin = skip * limit;
+  const end = skip * limit + limit;
+
+  return response.data.slice(begin, end);
+};
+
+export const getTodayDataApi = async (
+  skip: number,
+  limit: number,
+  today: string
+) => {
+  const response = await axios.get('/data/mockData.json');
+  const begin = skip * limit;
+  const end = skip * limit + limit;
+
+  const filterd = response.data.filter((item: DataType) =>
+    item.transaction_time.includes(today)
+  );
+
+  return filterd.slice(begin, end);
+};
