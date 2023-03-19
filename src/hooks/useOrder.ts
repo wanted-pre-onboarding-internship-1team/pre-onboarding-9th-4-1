e9f030765978acf57bda67dc704c067481880fac
dobidugi <dobidugi@gmail.com>
2023-03-19 22:23:10 +0900

    [#1] feat: useOrder 구현
    주문목록가져오는 custom hook

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,25 @@
+import { fetchOrders } from '../apis/fetchOrders';
+import { Order } from '../types/Order';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+
+export default function useOrder({
+  currentPage = 1,
+  limit,
+}: {
+  currentPage: number;
+  limit: number;
+}) {
+  const { data } = useQuery<Order[]>(['fetchOrderList'], () => fetchOrders(), {
+    useErrorBoundary: false,
+    retryDelay: 5000,
+  });
+  console.log('currentPage', currentPage);
+  const TODAY = '2023-03-08';
+  const todayData = data?.filter(
+    item => item.transaction_time.split(' ')[0] === TODAY
+  );
+  return {
+    data: todayData,
+  };
+}
