b6456071cd117dd226a68f621537d8bbc2814151
SeungYn <kwls0407@gmail.com>
2023-03-20 00:53:01 +0900

    [#1] feat: 리액트 쿼리 설정 및 mock타입 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import App from './App';
+import {
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+  QueryOptions,
+} from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,8 +12,23 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const quertOptions: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 5,
+      cacheTime: 1000 * 5,
+      refetchOnMount: false,
+      refetchInterval: 1000 * 5,
+    },
+  },
+};
+
+const queryClient = new QueryClient(quertOptions);
 root.render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
 );
