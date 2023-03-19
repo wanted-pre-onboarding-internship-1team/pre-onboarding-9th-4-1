ba28a47379dc011d5060f578ca4d647fb78b5131
dobidugi <dobidugi@gmail.com>
2023-03-19 21:13:04 +0900

    [#1] feat: Order타입 정의

---
+++ src/types/Order.ts
@@ -0,0 +1,17 @@
+/**
+ * Order interface
+ * @property {number} id - 주문번호
+ * @property {string} transaction_time - 거래시간
+ * @property {boolean} status - 주문처리상태
+ * @property {number} customer_id - 고객번호
+ * @property {string} custoer_name - 고객명
+ * @property {string} currency - 가격
+ */
+export interface Order {
+  id: number;
+  transaction_time: string;
+  status: boolean;
+  customer_id: number;
+  custoer_name: string;
+  currency: string;
+}
