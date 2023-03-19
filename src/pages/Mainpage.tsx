b8d7e445d9bfc1a5276700e02dc87f11b4054b31
yundonghui <qeadzc9503@naver.com>
2023-03-20 03:56:48 +0900

    [#1] Feat: 주문목록 스타일 추가

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -2,14 +2,21 @@ import Center from '../component/common/Center';
 import TransactionList from '../component/transaction/TransactionList';
 import { queryClient } from '../utils/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
+import styled from 'styled-components';
 
 export default function Mainpage() {
   return (
     <QueryClientProvider client={queryClient}>
       <Center height='100vh'>
-        <div>주문 목록</div>
+        <Head>주문 목록</Head>
         <TransactionList />
       </Center>
     </QueryClientProvider>
   );
 }
+
+const Head = styled.div`
+  font-weight: bold;
+  font-size: 34px;
+  padding-bottom: 20px;
+`;
