ab0079f605ed2a688df62cda274d72bc2a971f7b
yundonghui <qeadzc9503@naver.com>
2023-03-19 17:36:16 +0900

    [#1] Memo: todo 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+/**
+ * 
+ * TODO : cache, stale time (5초) 로 변경하기
+ * TODO : Suspense, Error Fallback 추가하기
+ */
+
+
 import Router from './router/Router';
-import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
