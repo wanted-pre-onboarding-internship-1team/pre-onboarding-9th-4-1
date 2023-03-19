f32ad74f084bff426f149d5470f970b30eb089ea
yundonghui <qeadzc9503@naver.com>
2023-03-19 14:39:49 +0900

    [#1] Feat: 테이블 스타일 추가

--- src/component/transaction/Transaction.tsx
+++ src/component/transaction/Transaction.tsx
@@ -1,16 +1,19 @@
 import { TX } from '../../interface/Transaction';
 import { parseDate } from '../../utils/date';
+import styled from 'styled-components';
 
 export default function Transaction({ tx }: { tx: TX }) {
   //   console.log();
   return (
-    <li>
-      <span>주문번호 : {tx.id}</span>
-      <span>고객번호 : {tx.customer_id}</span>
-      <span>거래시간 : {parseDate(tx.transaction_time)}</span>
-      <span>고객명 : {tx.customer_name}</span>
-      <span>가격 : {tx.currency}</span>
-      <span>주문 처리 상태 : {tx.status ? '주문 완료' : '주문 전'}</span>
-    </li>
+    <TableRow>
+      <RowColumn>{tx.id}</RowColumn>
+      <RowColumn>{tx.customer_id}</RowColumn>
+      <RowColumn>{tx.customer_name}</RowColumn>
+      <RowColumn>{tx.currency}</RowColumn>
+      <RowColumn>{parseDate(tx.transaction_time)}</RowColumn>
+      <RowColumn>{tx.status ? '주문 완료' : '주문 전'}</RowColumn>
+    </TableRow>
   );
 }
+const TableRow = styled.tr``;
+const RowColumn = styled.td``;
--- src/component/transaction/TransactionList.tsx
+++ src/component/transaction/TransactionList.tsx
@@ -3,6 +3,7 @@ import { TX } from '../../interface/Transaction';
 import Transaction from './Transaction';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import styled from 'styled-components';
 
 export async function fetchTx() {
   return await axios.get(TX_LIST).then(res => res.data);
@@ -20,10 +21,25 @@ export default function TransactionList() {
   if (error) return <>'An error has occurred: ' + error</>;
 
   return (
-    <ul>
-      {txList.map((tx: TX) => {
-        return <Transaction tx={tx} key={tx.id} />;
-      })}
-    </ul>
+    <Table>
+      <TableHeader>
+        <HeaderColumn>주문번호</HeaderColumn>
+        <HeaderColumn>고객번호</HeaderColumn>
+        <HeaderColumn>고객명</HeaderColumn>
+        <HeaderColumn>가격</HeaderColumn>
+        <HeaderColumn>거래시간</HeaderColumn>
+        <HeaderColumn>주문 처리 상태</HeaderColumn>
+      </TableHeader>
+      <TableBody>
+        {txList.map((tx: TX) => {
+          return <Transaction tx={tx} key={tx.id} />;
+        })}
+      </TableBody>
+    </Table>
   );
 }
+
+const Table = styled.table``;
+const TableHeader = styled.thead``;
+const HeaderColumn = styled.th``;
+const TableBody = styled.tbody``;
