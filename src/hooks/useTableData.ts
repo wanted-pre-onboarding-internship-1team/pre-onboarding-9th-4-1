536637482c83373a2212356caf205bb64e057d1e
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-03-20 04:39:52 +0900

    [#1] fix: 테이블 헤더 한글 명칭으로 변경

--- src/hooks/useTableData.ts
+++ src/hooks/useTableData.ts
@@ -7,8 +7,21 @@ const useTableData = (dataList: DataType[]) => {
   const columns = useMemo<ColumnDef<DataType>[]>(() => {
     if (!dataList) return [];
 
+    type DataHeaderType = {
+      [index: string]: string;
+    };
+
+    const dataHeader: DataHeaderType = {
+      id: '주문번호',
+      transaction_time: '거래시간',
+      status: '주문처리상태',
+      customer_id: '고객번호',
+      customer_name: '고객이름',
+      currency: '가격',
+    };
+
     return Object.keys(dataList[0]).map(key => ({
-      header: key,
+      header: dataHeader[key],
       accessorKey: key,
       cell: info => {
         const value = info.getValue() as string | number;
