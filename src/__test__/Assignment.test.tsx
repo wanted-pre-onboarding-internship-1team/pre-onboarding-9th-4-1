ca3e5fb75182cb12d110bcad914bf7359495835a
Kim Yo Han <rladygks329@gmail.com>
2023-03-23 13:08:46 +0900

    [#6] feat: assignment1 세부사항 test

--- src/__test__/Assignment.test.tsx
+++ src/__test__/Assignment.test.tsx
@@ -17,9 +17,18 @@ describe('Assignment test', () => {
     );
 
     await waitFor(() => {
+      //주문 목록 페이지에는 주문에 대한 모든 정보를 표 형태로 확인할 수 있어야 합니다.
+      const table = screen.getByRole('table');
+      expect(table).toBeInTheDocument();
+
+      //주문 목록은 페이지네이션이 구현되어야 합니다(한 페이지에 50건의 주문이 보여야 합니다)
       const numOfTable = screen.getByTestId('tbody').children.length;
       expect(numOfTable).toBe(50);
     });
-    // await screen.debug(undefined, Infinity);
+
+    //- 이터 중에서 오늘의 거래건만 보여지도록 해주세요
+    //여기서 오늘은 **“2023-03-08”**일을 의미합니다.
+    const numOfToday = await screen.findAllByText(/2023-03-08/);
+    expect(numOfToday.length).toBe(51); //headder: Today:2023-03-8 exist
   });
 });
